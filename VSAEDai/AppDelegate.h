ec7d72a06b9aa9a3334be0b11630b58098f164bf
alvinding <alvindc03@sina.com>
2015-09-17 09:58:19 +0800

    Initial Commit

---
+++ VSAEDai/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VSAEDai
+//
+//  Created by alvin on 15/9/17.
+//  Copyright (c) 2015年 alvin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
